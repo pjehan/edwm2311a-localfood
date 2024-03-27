0c8e6e77aeb24ac95111bedad1b11bfdfdc1aa9c
pjehan <pierre.jehan@gmail.com>
2024-03-27 10:42:01 +0100

    Utilisation de la commande make:crud sur l'entité Place

---
+++ src/Form/PlaceType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\City;
+use App\Entity\Place;
+use App\Entity\Type;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlaceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('type', EnumType::class, [
+                'class' => Type::class,
+                'choice_label' => 'value',
+            ])
+            ->add('address')
+            ->add('categories', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+            ])
+            ->add('owner', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'email',
+            ])
+            ->add('city', EntityType::class, [
+                'class' => City::class,
+                'choice_label' => 'name',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Place::class,
+        ]);
+    }
+}
