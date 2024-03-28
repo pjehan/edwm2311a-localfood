838fc770f63db232b7f7bb1bd85455ffc536ee1e
pjehan <pierre.jehan@gmail.com>
2024-03-28 11:02:31 +0100

    Affichage de la liste des catégories dans le header (render controller)

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,7 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import { Dropdown } from "bootstrap";
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootswatch/dist/minty/bootstrap.min.css';
 import './styles/app.css';
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,5 +13,12 @@ class MainController extends AbstractController
     public function home(): Response
     {
         return $this->render('main/index.html.twig');
+    }
+
+    public function dropdownCategories(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('main/_dropdownCategories.html.twig', [
+            'categories' => $categoryRepository->findAll()
+        ]);
     }
 }
